ad4391b52b0c13909f57f0b0f03db51fea600c7c
Duraspinvest <emilio@duraspinvest.com>
2026-09-09 13:30:29 +0200

    Add files via upload

--- grupo/sw.js
+++ grupo/sw.js
@@ -6,7 +6,7 @@
       desplegada y el dispositivo tenga token FCM registrado.
 */
 
-const CACHE = 'grupo-v1';
+const CACHE = 'grupo-v2';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
